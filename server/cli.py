ce68489136cf49f54aed37fc13bf8762f57165bc
Andrew <git@andrewscaccetti.com>
2026-06-01 15:17:27 -0400

    feat: update gunicorn binding to support both IPv4 and IPv6 addresses

--- server/cli.py
+++ server/cli.py
@@ -13,11 +13,13 @@ def run_prod():
     load_dotenv(dotenv_path=".flaskenv")
     load_dotenv()
     
-    host = os.environ.get("FLASK_RUN_HOST", "0.0.0.0")
     port = os.environ.get("FLASK_RUN_PORT", "8000")
-    bind_address = f"{host}:{port}"
     
     try:
-        subprocess.run(["gunicorn", "server.app:app", "--bind", bind_address], check=True)
+        subprocess.run([
+            "gunicorn", "server.app:app",
+            "--bind", f"0.0.0.0:{port}",
+            "--bind", f"[::]:{port}",
+        ], check=True)
     except KeyboardInterrupt:
         sys.exit(0)
